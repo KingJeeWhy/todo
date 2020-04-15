49e086f48bb8479ec5f108e06ad1bf110e9cee3f
KingJeeWhy <361377516@qq.com>
2020-04-15 20:41:53 +0800

    todo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello.wsgi
+web: gunicorn todo.wsgi --log-file -
--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vhjrmjmy(ja(n)!o0n-(g1h7ff7cik#)dl10wjoo1l-e=d2l4g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
